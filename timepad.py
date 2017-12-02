d09e0fef5356cb24c9cd613248a505da51dd1095
Andrey Putilin <regresscheck@gmail.com>
2017-12-02 12:43:59 +0300

    code style

--- timepad.py
+++ timepad.py
@@ -4,19 +4,21 @@ import json
 
 API_URL = 'https://api.timepad.ru'
 
+
 def get_user_events(user_token):
-	response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
-	user_info = json.loads(response.text)
-	# user_id = user_info['user_id']
-	event_ids = [order['event']['id'] for order in user_info['orders']]
-	for event_id in event_ids:
-		response = requests.get(API_URL + '/v1/events/{0}?fields=name&token={1}'.format(event_id, user_token))
-		if response.status_code != requests.codes.ok:
-			logging.warning('Private event: {0}'.format(event_id))
-			continue
-		event_info = json.loads(response.text)
-		print(event_info['name'])	
-	return event_ids
+    response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
+    user_info = json.loads(response.text)
+    # user_id = user_info['user_id']
+    event_ids = [order['event']['id'] for order in user_info['orders']]
+    for event_id in event_ids:
+        response = requests.get(API_URL + '/v1/events/{0}?fields=name&token={1}'.format(event_id, user_token))
+        if response.status_code != requests.codes.ok:
+            logging.warning('Private event: {0}'.format(event_id))
+            continue
+        event_info = json.loads(response.text)
+        print(event_info['name'])
+    return event_ids
+
 
 def introspect(token):
     payload = {
@@ -29,5 +31,6 @@ def introspect(token):
         return None
     return json.loads(response.text)
 
+
 if __name__ == '__main__':
-	print(get_user_events('9a9bc7b393f50a06297e80e6b5bf9d9fba23b351')) # aq token
+    print(get_user_events('9a9bc7b393f50a06297e80e6b5bf9d9fba23b351'))  # aq token
