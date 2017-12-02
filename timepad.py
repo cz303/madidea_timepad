7e6e012274dbd8d707b821e953aa11898fc04f3e
Alexey Kulikov <alexeyqu@gmail.com>
2017-12-02 12:58:08 +0300

    all vs active user events

--- timepad.py
+++ timepad.py
@@ -4,7 +4,7 @@ import json
 
 API_URL = 'https://api.timepad.ru'
 
-def get_user_events(user_token):
+def get_all_user_events(user_token):
 	response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
 	user_info = json.loads(response.text)
 	# user_id = user_info['user_id']
@@ -15,19 +15,27 @@ def get_user_events(user_token):
 			logging.warning('Private event: {0}'.format(event_id))
 			continue
 		event_info = json.loads(response.text)
-		print(event_info['name'])	
+		print(event_info['name'])
+	return event_ids
+
+
+def get_user_events(user_token):
+	response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
+	user_info = json.loads(response.text)
+	event_ids = [order['event']['id'] for order in user_info['orders']]
+	print(requests.get(API_URL + '/v1/events/?event_ids={0}&access_statuses=public&starts_at_min=now'.format(','.join(str(id) for id in event_ids))).text)
 	return event_ids
 
 def introspect(token):
-    payload = {
-        'token': token
-    }
-    response = requests.get(API_URL + '/introspect', params=payload)
-    if response.status_code != requests.codes.ok:
-        logging.warning('Got non-200 response from API: {}'.format(str(response.status_code)))
-        logging.warning(response.text)
-        return None
-    return json.loads(response.text)
+	payload = {
+		'token': token
+	}
+	response = requests.get(API_URL + '/introspect', params=payload)
+	if response.status_code != requests.codes.ok:
+		logging.warning('Got non-200 response from API: {}'.format(str(response.status_code)))
+		logging.warning(response.text)
+		return None
+	return json.loads(response.text)
 
 if __name__ == '__main__':
 	print(get_user_events('9a9bc7b393f50a06297e80e6b5bf9d9fba23b351')) # aq token
