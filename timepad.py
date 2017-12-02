681cc955bbd5353a07944d9ba6be1a35d29f34cb
Andrey Putilin <regresscheck@gmail.com>
2017-12-02 13:04:59 +0300

    code style

--- timepad.py
+++ timepad.py
@@ -5,38 +5,41 @@ import datetime
 
 API_URL = 'https://api.timepad.ru'
 
+
 def get_all_user_events(user_token):
-	response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
-	user_info = json.loads(response.text)
-	# user_id = user_info['user_id']
-	event_ids = [order['event']['id'] for order in user_info['orders']]
-	for event_id in event_ids:
-		response = requests.get(API_URL + '/v1/events/{0}?fields=name&token={1}'.format(event_id, user_token))
-		if response.status_code != requests.codes.ok:
-			logging.warning('Private event: {0}'.format(event_id))
-			continue
-		event_info = json.loads(response.text)
-		print(event_info['name'])
-	return event_ids
+    response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
+    user_info = json.loads(response.text)
+    # user_id = user_info['user_id']
+    event_ids = [order['event']['id'] for order in user_info['orders']]
+    for event_id in event_ids:
+        response = requests.get(API_URL + '/v1/events/{0}?fields=name&token={1}'.format(event_id, user_token))
+        if response.status_code != requests.codes.ok:
+            logging.warning('Private event: {0}'.format(event_id))
+            continue
+        event_info = json.loads(response.text)
+        print(event_info['name'])
+    return event_ids
 
 
 def get_user_events(user_token):
-	response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
-	user_info = json.loads(response.text)
-	event_ids = [order['event']['id'] for order in user_info['orders']]
-	print(requests.get(API_URL + '/v1/events/?event_ids={0}&access_statuses=public&starts_at_min=now'.format(','.join(str(id) for id in event_ids))).text)
-	return event_ids
+    response = requests.get(API_URL + '/introspect?token={0}'.format(user_token))
+    user_info = json.loads(response.text)
+    event_ids = [order['event']['id'] for order in user_info['orders']]
+    print(requests.get(API_URL + '/v1/events/?event_ids={0}&access_statuses=public&starts_at_min=now'.format(
+        ','.join(str(id) for id in event_ids))).text)
+    return event_ids
+
 
 def introspect(token):
-	payload = {
-		'token': token
-	}
-	response = requests.get(API_URL + '/introspect', params=payload)
-	if response.status_code != requests.codes.ok:
-		logging.warning('Got non-200 response from API: {}'.format(str(response.status_code)))
-		logging.warning(response.text)
-		return None
-	return json.loads(response.text)
+    payload = {
+        'token': token
+    }
+    response = requests.get(API_URL + '/introspect', params=payload)
+    if response.status_code != requests.codes.ok:
+        logging.warning('Got non-200 response from API: {}'.format(str(response.status_code)))
+        logging.warning(response.text)
+        return None
+    return json.loads(response.text)
 
 
 def get_events_by_date(date=datetime.datetime.today().strftime('%Y-%m-%d')):
